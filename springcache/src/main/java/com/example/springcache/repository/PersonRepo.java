9160da6684a2d1ae7b2e9711e18ed2b0b9952ffc
delalama <elaltas@gmail.com>
2018-09-24 18:17:11 +0200

    Add files via upload

--- springcache/src/main/java/com/example/springcache/repository/PersonRepo.java
+++ springcache/src/main/java/com/example/springcache/repository/PersonRepo.java
@@ -1,9 +1,17 @@
 package com.example.springcache.repository;
 
 import com.example.springcache.entities.Person;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepo extends CrudRepository<Person, Long> {
+
+    @Override
+    @Cacheable("persons")
+    public List<Person> findAll();
+
 }
